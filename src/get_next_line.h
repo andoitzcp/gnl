106938fab30306ca06b114149ec852e742dde8d6
andoitzcp <andoitzcp@gmail.com>
2023-01-17 12:09:09 +0100

    project handle

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -6,24 +6,22 @@
 /*   By: acampo-p <acampo-p@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 20:30:52 by acampo-p          #+#    #+#             */
-/*   Updated: 2023/01/15 14:22:53 by acampo-p         ###   ########.fr       */
+/*   Updated: 2023/01/16 22:50:11 by acampo-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
 
 //#define BUFFER_SIZE 42
-
-typedef struct	s_bufr
-{
-	char	*str;
-	size_t	indx;
-	ssize_t	szrd;
-}				t_bufr;
 
 ssize_t	ft_nextnlpos(const void *start);
 char	*get_next_line(int fd);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *str);
+
+#endif
